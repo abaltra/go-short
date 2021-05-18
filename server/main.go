d5f6689b8be45c7bc3d8228787df80b551864ca4
Alejandro Baltra <abaltra@gmail.com>
2021-05-18 00:27:30 -0400

    Added Dockerfiles and docker compose setup

--- server/main.go
+++ server/main.go
@@ -124,10 +124,11 @@ func main() {
 
 	srv := &http.Server{
 		Handler: handler,
-		Addr:    "127.0.0.1:8000",
+		Addr:    "0.0.0.0:8000",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
+	log.Printf("Starting server at 0.0.0.0:8000")
 	log.Fatal(srv.ListenAndServe())
 }
